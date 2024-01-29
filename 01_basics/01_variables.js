68af141fbe27e8b96b2716f3943111e6b2e0de6f
Divyansh <divyanshxsachdeva@gmail.com>
2024-01-29 06:16:55 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const name = "Divyansh";
+let id = 12219096;
+var email = "divyansh@google.com"
+// avoid using var due to issues in scope
+console.table([name, id, email]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treats code as newer version of js code
+
+// alert(6+5) // it is node
+
+// DATA TYPES IN JS
+// number 
+// bigint
+// string 
+// boolean
+// null => holds a empty value
+// undefined
+// symbol => will be useful while learning react
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
