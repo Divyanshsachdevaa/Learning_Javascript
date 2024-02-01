af2d860065b43f91794d6c74d2fd4ab0adffd285
Divyansh <divyanshxsachdeva@gmail.com>
2024-02-01 08:59:45 +0000

    conversion

--- 01_basics/dataTypes_conversions.js
+++ 01_basics/dataTypes_conversions.js
@@ -1,9 +1,9 @@
 // MAINLY TWO TYPES OF DATA TYPES ARE THERE IN JS:- PRIMITIVE & NON-PRIMITIVE
 
-// PRIMITIVE - WHICH MAKE COPY
+// PRIMITIVE - WHICH MAKE COPY - IT TAKES STACK MEMORY
 // int, string, char, null, undefined, BigInt, boolean
 
-// NON-PRIMITIVE - WHICH PASS BY REFERNCE
+// NON-PRIMITIVE - WHICH PASS BY REFERNCE - IT TAKES HEAP MEMORY
 // arrays, objects, functions
 
 const collectionOfNumbers = [1, 2, 3, 4, 5];
